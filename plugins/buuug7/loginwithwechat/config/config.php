cffcdc491f544a45f30da98d0848b8db48022cdd
buuug7 <youpp@126.com>
2018-01-17 16:48:31 +0800

    update

--- plugins/buuug7/loginwithwechat/config/config.php
+++ plugins/buuug7/loginwithwechat/config/config.php
@@ -4,6 +4,6 @@ return [
     'wechat' => [
         'AppID' => 'wxce7fc7442ed90d91',
         'AppSecret' => '1236bf050255a68b9a818d0b3be6203f',
-        'redirect_uri' => 'yoursoups.com/login/wechat/callback',
+        'redirect_uri' => 'https://yoursoups.com/login/wechat/callback',
     ],
 ];
